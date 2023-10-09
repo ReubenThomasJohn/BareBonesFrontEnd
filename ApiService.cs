0fe6b358697ff4eaa98bc9ab138e6a3f8034e83a
Reuben Thomas <reubenthomas.john2017@gmail.com>
2023-10-09 14:23:10 +0530

    add CRUD methods

--- ApiService.cs
+++ ApiService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using Newtonsoft.Json;
 using StudentFrontEnd.Models;
@@ -13,7 +14,7 @@ public class ApiService
     {
         _httpClient = httpClient;
         _httpClient.BaseAddress = new Uri("http://localhost:5209/");
-        
+
         _httpClient.DefaultRequestHeaders.Accept.Clear();
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     }
@@ -27,8 +28,42 @@ public class ApiService
             string? stringResponse = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<List<Student>>(stringResponse);
         }
-        else {
+        else
+        {
             return new List<Student>();
+        }
+    }
+
+    public async Task PostDataAsync(string endpoint, Student student)
+    {
+        var jsonData = JsonConvert.SerializeObject(student);
+
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync(endpoint, content);
+    }
+
+    public async Task DeleteDataAsync(string endpoint)
+    {
+        var response = await _httpClient.DeleteAsync(endpoint);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            // Handle error cases here, e.g., throw an exception or log an error
+        }
+    }
+
+    public async Task UpdateDataAsync(string endpoint, Student student)
+    {
+        var jsonData = JsonConvert.SerializeObject(student);
+
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PutAsync(endpoint, content);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            // Handle error cases here, e.g., throw an exception or log an error
         }
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,10 +13,43 @@ public class HomeController : Controller
         _apiService = apiService;
     }
 
+    [HttpGet]
     public async Task<IActionResult> Index()
     {
         var studentData = await _apiService.GetResourceDataAsync("students/");
         return View(studentData);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Student student)
+    {
+        // Make a POST request to create a new student
+        await _apiService.PostDataAsync("/api/students", student);
+
+        // Handle the response as needed (e.g., redirect to a different page)
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> Delete(int id)
+    {
+        var endpoint = $"/students/{id}";
+        await _apiService.DeleteDataAsync(endpoint);
+
+        // Handle the response as needed (e.g., redirect to a different page)
+        return RedirectToAction("Index");
+    }
+
+    public async Task<IActionResult> UpdateStudent(int id, Student updatedStudent)
+    {
+        var endpoint = $"/api/students/{id}";
+        await _apiService.UpdateDataAsync(endpoint, updatedStudent);
+        return RedirectToAction("Index");
     }
 
     public IActionResult Privacy()
